3d7a5939a33e1b1b6c2af6d06e1456e51f248250
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-04-22 21:16:42 +0000

    100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,14 +1,14 @@
 export default function* createIteratorObject(report) {
-    if (report && report.allEmployees) {
-        for (const department in report.allEmployees) {
-            if (report.allEmployees.hasOwnProperty(department)) {
-                const employeeList = report.allEmployees[department];
-                if (Array.isArray(employeeList)) {
-                    for (const employee of employeeList) {
-                        yield employee;
-                    }
-                }
-            }
+  if (report && report.allEmployees) {
+    for (const department in report.allEmployees) {
+      if (Object.prototype.hasOwnProperty.call(report.allEmployees, department)) {
+        const employeeList = report.allEmployees[department];
+        if (Array.isArray(employeeList)) {
+          for (const employee of employeeList) {
+            yield employee;
+          }
         }
+      }
     }
+  }
 }
