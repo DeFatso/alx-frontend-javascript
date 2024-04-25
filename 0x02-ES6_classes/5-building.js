c4575f6804ba0eeaf8a179a83fe3c698df6ccca4
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-04-25 17:42:15 +0000

    first commit

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+    constructor(sqft) {
+        if (new.target === Building) {
+            throw new Error('Building cannot be instantiated directly');
+        }
+
+        this._sqft = sqft;
+
+        if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+    }
+
+    get sqft() {
+        return this._sqft;
+    }
+
+    evacuationWarningMessage() {
+        throw new Error('Method evacuationWarningMessage() must be implemented');
+    }
+}
+
